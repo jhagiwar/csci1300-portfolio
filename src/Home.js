d811e570376e47b58db4f348d427badbe135fb06
jhagiwar <58452172+jhagiwar@users.noreply.github.com>
2022-12-12 23:54:34 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,17 +4,17 @@ import React from 'react'
 
 const projectData = [
     {
-        name: "Who Spins There?", image: "images/small_scooter.jpg", href: "/csci1300-portfolio/PersonasAndStoryboard", shortDescription: "Come with me on my journey to better understand the users of spin scooters! This includes interview summaries, user personas, and a storyboard"
+        name: "Who Spins There?", image: "images/small_scooter.jpg", href: "/csci1300-portfolio-personas", shortDescription: "Come with me on my journey to better understand the users of spin scooters! This includes interview summaries, user personas, and a storyboard"
     },
     {
-        name: "MetroNails Redesign", image: "images/responsiveredesigncover.png", href: "/csci1300-portfolio/ResponsiveRedesign", shortDescription: "Makeover time! Take a look at this redesign of the MetroNails website, the website of a Nail Salon in Providence"
+        name: "MetroNails Redesign", image: "images/responsiveredesigncover.png", href: "/csci1300-portfolio-responsive", shortDescription: "Makeover time! Take a look at this redesign of the MetroNails website, the website of a Nail Salon in Providence"
     },
     {
-        name: "Lancey", image: "images/lancey.png", href: "/csci1300-portfolio/IterativeDesign", shortDescription: "Come with me as I design an interface for Y Combinator startup Lancey!"
+        name: "Lancey", image: "images/lancey.png", href: "/csci1300-portfolio-iterative", shortDescription: "Come with me as I design an interface for Y Combinator startup Lancey!"
     }
     ,
     {
-        name: "My Super Great Candle Shop", image: "images/candle.jpg", href: "/csci1300-portfolio/Development", shortDescription: "Want to browse some delightful candles? This was my first interactive interface and is a shopping page for a fictional candle shop."
+        name: "My Super Great Candle Shop", image: "images/candle.jpg", href: "/csci1300-portfolio-development", shortDescription: "Want to browse some delightful candles? This was my first interactive interface and is a shopping page for a fictional candle shop."
     },
 ]
 
